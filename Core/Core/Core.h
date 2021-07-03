0d3e4fc43b534510cfa95595d23a39a8589aa62e
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-07-03 16:41:31 +0200

    Create Core module

---
+++ Core/Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Sameh Mabrouk on 03/07/2021.
+//  Copyright © 2021 Sameh Mabrouk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
