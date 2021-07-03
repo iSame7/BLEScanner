ea15c75ae82663c82fbf28f45bfd9280bd5e5be8
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-07-03 15:57:26 +0200

    Create BLEScanner project

---
+++ BLEScanner/BLEScannerTests/BLEScannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  BLEScannerTests.swift
+//  BLEScannerTests
+//
+//  Created by Sameh Mabrouk on 03/07/2021.
+//  Copyright © 2021 Sameh Mabrouk. All rights reserved.
+//
+
+import XCTest
+@testable import BLEScanner
+
+class BLEScannerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
