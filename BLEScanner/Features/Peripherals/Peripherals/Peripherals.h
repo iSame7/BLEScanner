97193c7a24e5dce48e7372c153eb89478e291aa2
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-07-05 21:46:13 +0200

    Create Peripherals module

---
+++ BLEScanner/Features/Peripherals/Peripherals/Peripherals.h
@@ -0,0 +1,19 @@
+//
+//  Peripherals.h
+//  Peripherals
+//
+//  Created by Sameh Mabrouk on 05/07/2021.
+//  Copyright © 2021 Sameh Mabrouk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Peripherals.
+FOUNDATION_EXPORT double PeripheralsVersionNumber;
+
+//! Project version string for Peripherals.
+FOUNDATION_EXPORT const unsigned char PeripheralsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Peripherals/PublicHeader.h>
+
+
