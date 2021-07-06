ffcbcc12c2591f7bb2f845995e9fa3bf7b88f7f5
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-07-06 12:59:32 +0200

    Create AppRootCoordinator

--- BLEScanner/BLEScanner/App/AppRootCoordinator.swift
+++ BLEScanner/BLEScanner/App/AppRootCoordinator.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2021 Sameh Mabrouk. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Core
+import RxSwift
+import Peripherals
+
+class AppRootCoordinator: BaseCoordinator<Void> {
+    
+    private let window: UIWindow
+    private let locationsModuleBuilder: ModuleBuildable
+    
+    init(window: UIWindow, locationsModuleBuilder: ModuleBuildable) {
+        self.window = window
+        self.locationsModuleBuilder = locationsModuleBuilder
+    }
+    
+    override func start() -> Observable<Void> {
+        guard let locationsCoordinator: BaseCoordinator<Void> = locationsModuleBuilder.buildModule(with: window)?.coordinator else {
+            preconditionFailure("[AppCoordinator] Cannot get locationsModuleBuilder from module builder")
+        }
+        
+        _ = coordinate(to: locationsCoordinator).subscribe({ event in
+            
+        }).disposed(by: disposeBag)
+        return .never()
+    }
+}
