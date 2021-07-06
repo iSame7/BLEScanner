8e85c72ecca364ce357d51d2c9601fbc6b7bac27
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-07-06 13:14:37 +0200

    Create BlueKit

---
+++ BLEScanner/Common/BlueKit/BlueKit/BlueKit.h
@@ -0,0 +1,19 @@
+//
+//  BlueKit.h
+//  BlueKit
+//
+//  Created by Sameh Mabrouk on 06/07/2021.
+//  Copyright © 2021 Sameh Mabrouk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlueKit.
+FOUNDATION_EXPORT double BlueKitVersionNumber;
+
+//! Project version string for BlueKit.
+FOUNDATION_EXPORT const unsigned char BlueKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueKit/PublicHeader.h>
+
+
---
+++ BLEScanner/Common/BlueKit/BlueKitTests/BlueKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  BlueKitTests.swift
+//  BlueKitTests
+//
+//  Created by Sameh Mabrouk on 06/07/2021.
+//  Copyright © 2021 Sameh Mabrouk. All rights reserved.
+//
+
+import XCTest
+@testable import BlueKit
+
+class BlueKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
